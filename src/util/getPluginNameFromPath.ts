a605217712bcbf6e717a915caf68c29aa5e3de26
Jovan Jovanovic <jovanovicjovan921@gmail.com>
2024-01-08 03:27:13 +0100

    fix regex on windows

--- src/util/getPluginNameFromPath.ts
+++ src/util/getPluginNameFromPath.ts
@@ -1,6 +1,6 @@
 import { sep } from "path";
 
-const regex = new RegExp(`plugins${sep}(.+?)${sep}`);
+const regex = new RegExp(`plugins\\${sep}(.+?)\\${sep}`);
 
 export function getPluginNameFromPath(path: string): string {
   const match = regex.exec(path);
